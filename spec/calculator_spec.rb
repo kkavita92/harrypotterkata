4c062fe788dce834029f3cb1c67dee11e5c479c2
kkavita92 <kkavita92@gmail.com>
2017-09-02 22:05:45 +0800

    Measures price for basket with one book

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,6 +8,10 @@ describe Calculator do
       empty_basket = []
       expect(calculator.get_price(empty_basket)).to eq 0
     end
+
+    it 'calculates price for basket with one item' do
+      expect(calculator.get_price([1])).to eq 8 
+    end
   end
 
 end
