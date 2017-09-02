16f094e8ea2cefebf2114c11d23a772da8ad8725
kkavita92 <kkavita92@gmail.com>
2017-09-02 22:19:19 +0800

    Calculates price for basket with multiple copies of same book

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -14,7 +14,14 @@ describe Calculator do
       expect(calculator.get_price([2])).to eq 8
       expect(calculator.get_price([3])).to eq 8
       expect(calculator.get_price([4])).to eq 8
-      expect(calculator.get_price([5])).to eq 8 
+      expect(calculator.get_price([5])).to eq 8
+    end
+
+    it 'calculates price for baskets with multiple copies of same book' do
+      expect(calculator.get_price([1, 1])).to eq 16
+      expect(calculator.get_price([2, 2])).to eq 16
+      expect(calculator.get_price([4, 4, 4, 4])).to eq 32
+      expect(calculator.get_price([3, 3, 3, 3, 3])).to eq 40
     end
   end
 
