11bf1244cd77e69039260f5a4743bf80736ea07f
kkavita92 <kkavita92@gmail.com>
2017-09-06 20:11:57 +0800

    Include #full_price method to refactor price calculation

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,16 +2,20 @@ class Calculator
 
   def get_price(basket)
     return discount_price(basket) unless all_basket_same?(basket)
-    basket.length * 8
+    full_price(basket)
   end
 
   private
 
   def discount_price(basket)
-    return 0.95 * (basket.length * 8) if basket.length == 2
-    return 0.90 * (basket.length * 8) if basket.length == 3
-    return 0.80 * (basket.length * 8) if basket.length == 4
-    return 0.75 * (basket.length * 8) if basket.length == 5
+    return 0.95 * full_price(basket) if basket.length == 2
+    return 0.90 * full_price(basket) if basket.length == 3
+    return 0.80 * full_price(basket) if basket.length == 4
+    return 0.75 * full_price(basket) if basket.length == 5
+  end
+
+  def full_price(basket)
+    basket.length * 8
   end
 
   def all_basket_same?(basket)
