69eb3fb801b74c41f73126259fd04637f5d754b5
kkavita92 <kkavita92@gmail.com>
2017-09-07 23:57:31 +0800

    Replace book price with constant

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,14 +1,25 @@
 class Calculator
 
+  BOOK_PRICE = 8
+
   def get_price(basket)
     return discount_price(basket) unless all_basket_same?(basket)
     full_price(basket)
   end
 
+  def sort_basket(basket)
+    sorted = []
+
+    basket.each do |book|
+      sorted << book unless sorted.include?(book)
+    end
+
+  end
+
   private
 
   def discount_price(basket)
-    return discount_price(set(basket)) + number_of_repeated_titles(basket) * 8 if repeated_titles_included?(basket)
+    return discount_price(set(basket)) + number_of_repeated_titles(basket) * BOOK_PRICE if repeated_titles_included?(basket)
     return 0.95 * full_price(basket) if set(basket).length == 2
     return 0.90 * full_price(basket) if set(basket).length == 3
     return 0.80 * full_price(basket) if set(basket).length == 4
@@ -20,7 +31,7 @@ class Calculator
   end
 
   def full_price(basket)
-    basket.length * 8
+    basket.length * BOOK_PRICE
   end
 
   def all_basket_same?(basket)
