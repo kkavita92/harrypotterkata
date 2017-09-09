907abf87c13b23fd19652e666e4eac545a347ec9
kkavita92 <kkavita92@gmail.com>
2017-09-09 16:10:30 +0800

    Implement #sort_basket method to index basket

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,29 +1,43 @@
 class Calculator
 
   BOOK_PRICE = 8
+  BOOKS_INDEX = [1, 2, 3, 4, 5]
 
   def get_price(basket)
     return discount_price(basket) unless all_basket_same?(basket)
     full_price(basket)
   end
 
-  def sort_basket(basket)
-    sorted = []
-
-    basket.each do |book|
-      sorted << book unless sorted.include?(book)
-    end
-
-  end
-
   private
 
+  def sort_basket(basket)
+    arr = BOOKS_INDEX.map do |x|
+      basket.count(x)
+    end
+
+    set = []
+    while arr.max > 0
+      set << 5 - arr.count(0)
+      arr = arr.map do |count|
+        count > 0 ? count -= 1 : count
+      end
+    end
+
+     set
+  end
+
   def discount_price(basket)
-    return discount_price(set(basket)) + number_of_repeated_titles(basket) * BOOK_PRICE if repeated_titles_included?(basket)
-    return 0.95 * full_price(basket) if set(basket).length == 2
-    return 0.90 * full_price(basket) if set(basket).length == 3
-    return 0.80 * full_price(basket) if set(basket).length == 4
-    return 0.75 * full_price(basket) if set(basket).length == 5
+    sorted = sort_basket(basket)
+    sorted.map do |x|
+      x == 1 ? x * 8 : count(x)
+    end.reduce(:+)
+  end
+
+  def count(x)
+    return 0.95 * 16 if x == 2
+    return 0.90 * 24 if x == 3
+    return 0.80 * 32 if x == 4
+    return 0.75 * 40 if x == 5
   end
 
   def set(basket)
@@ -36,14 +50,6 @@ class Calculator
 
   def all_basket_same?(basket)
     basket.all? {|book| book == basket[0]}
-  end
-
-  def repeated_titles_included?(basket)
-    basket.uniq.length != basket.length
-  end
-
-  def number_of_repeated_titles(basket)
-    basket.length - basket.uniq.length
   end
 
 end
