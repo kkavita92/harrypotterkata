306d73fab53b017da698b0a24df1c4d78f0025bb
kkavita92 <kkavita92@gmail.com>
2017-09-09 16:56:48 +0800

    Extract calculating discount logic to DiscountCalculator class

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,13 +1,13 @@
 require_relative 'maximum_discount_finder'
+require_relative 'discount_calculator'
 
 class Calculator
 
   BOOK_PRICE = 8
-
-  attr_reader :discount_finder
-
-  def initialize(discount_finder = MaximumDiscountFinder)
+  
+  def initialize(discount_finder = MaximumDiscountFinder, discount_calculator = DiscountCalculator)
     @discount_finder = discount_finder.new
+    @discount_calculator = discount_calculator.new
   end
 
   def get_price(basket)
@@ -23,16 +23,7 @@ class Calculator
 
   def discount_price(basket)
     indexed_basket = divide_basket_into_discountable_sets(basket)
-    indexed_basket.map do |no_of_books_in_set|
-      no_of_books_in_set == 1 ? no_of_books_in_set * BOOK_PRICE : calculate_discount(no_of_books_in_set)
-    end.reduce(:+)
-  end
-
-  def calculate_discount(no_of_books_in_set)
-    return 0.95 * 16 if no_of_books_in_set == 2
-    return 0.90 * 24 if no_of_books_in_set == 3
-    return 0.80 * 32 if no_of_books_in_set == 4
-    return 0.75 * 40 if no_of_books_in_set == 5
+    @discount_calculator.get_total_discount(indexed_basket)
   end
 
   def full_price(basket)
---
+++ lib/discount_calculator.rb
@@ -0,0 +1,18 @@
+class DiscountCalculator
+
+  BOOK_PRICE = 8
+
+  def get_total_discount(indexed_basket)
+    indexed_basket.map do |no_of_books_in_set|
+      no_of_books_in_set == 1 ? no_of_books_in_set * BOOK_PRICE : calculate_discount(no_of_books_in_set)
+    end.reduce(:+)
+  end
+
+  def calculate_discount(no_of_books_in_set)
+    return 0.95 * 16 if no_of_books_in_set == 2
+    return 0.90 * 24 if no_of_books_in_set == 3
+    return 0.80 * 32 if no_of_books_in_set == 4
+    return 0.75 * 40 if no_of_books_in_set == 5
+  end
+
+end
