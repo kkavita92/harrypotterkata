89e3a96b6d175a5bb2e8a7fe9a8f0d4f1fcba9e3
kkavita92 <kkavita92@gmail.com>
2017-09-12 00:05:46 +0800

    Further refactoring to make #calculate_discount method an one liner wohoo

--- lib/discount_calculator.rb
+++ lib/discount_calculator.rb
@@ -1,9 +1,6 @@
 class DiscountCalculator
   BOOK_PRICE = 8
-  TWO_BOOK_DISCOUNT = 0.05
-  THREE_BOOK_DISCOUNT = 0.10
-  FOUR_BOOK_DISCOUNT = 0.20
-  FIVE_BOOK_DISCOUNT = 0.25
+  DISCOUNTS = [0, 0.05, 0.10, 0.20, 0.25]
 
   def get_total_discount(indexed_basket)
     indexed_basket.map do |no_of_books_in_set|
@@ -14,26 +11,6 @@ class DiscountCalculator
   private
 
   def calculate_discount(no_of_books_in_set)
-    return BOOK_PRICE if no_of_books_in_set == 1
-    return two_book_discount if no_of_books_in_set == 2
-    return three_book_discount if no_of_books_in_set == 3
-    return four_book_discount if no_of_books_in_set == 4
-    return five_book_discount if no_of_books_in_set == 5
-  end
-
-  def two_book_discount
-    return (2 * BOOK_PRICE) * (1 - TWO_BOOK_DISCOUNT)
-  end
-
-  def three_book_discount
-    return (3 * BOOK_PRICE) * (1 - THREE_BOOK_DISCOUNT)
-  end
-
-  def four_book_discount
-    return (4 * BOOK_PRICE) * (1 - FOUR_BOOK_DISCOUNT)
-  end
-
-  def five_book_discount
-    return (5 * BOOK_PRICE) * (1 - FIVE_BOOK_DISCOUNT)
+    (1 - DISCOUNTS[no_of_books_in_set - 1]) * (no_of_books_in_set * BOOK_PRICE)
   end
 end
